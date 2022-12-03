997ab8231515b1af9a65462c2b4bcefb5bc66668
AleSavarin <savaing@hotmail.com>
2022-12-03 05:06:33 -0300

    prueba insertar registro

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask                 # para utilizar el FrameWork
 from flask import render_template       # renderizar los templates
+from flaskext.mysql import MySQL        # para ejecutar consultas SQL
 
 app = Flask(__name__)                   # mi app va a ser de Flask y por costumbre se utiliza __name__
 
-# Acá todo el código que necesito!
+# Conexión a la BBDD
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'       # podría ser un host remoto (IP o URL) 
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''            # Creo que no puse contraseña
+app.config['MYSQL_DATABASE_DB']='sistema22515'      # Nombre de la BBDD
 
-@app.route('/')             # Cuando voy al puerto ####/ hago
+mysql.init_app(app)             # Para iniciar la conexión.
+
+
+@app.route('/')                 # Cuando voy al puerto ####/ hago
 def index():
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Alejandro', 'savaing@hotmail.com', 'fotoAlejandro.jpg');"
+    conn = mysql.connect()      # abro la conexión con la BBDD
+    cursor = conn.cursor()      # para que vaya sobre la BBDD
+    cursor.execute(sql)         # le paso la consulta SQL
+    conn.commit()               # finaliza la acción y actualiza 
+
     return render_template('empleados/index.html')      # renderizo index.html
 
-'''
-@app.route('/juanca')      
-def juanca():
-    return render_template('empleados/juanca.html')
-'''
+
 
 
 # al final hago el punto de entrada a la app
